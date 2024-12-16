b0383185438788226519926e3ba3d57f5e2268af
Nathan BOSCHI <nathanboschi25@gmail.com>
2024-12-16 11:14:27 +0100

    change logo

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,7 +12,7 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('groups')" :active="request()->routeIs('dashboard')">
+                    <x-nav-link :href="route('groups')" :active="request()->routeIs('groups')">
                         {{ __('Groups') }}
                     </x-nav-link>
                 </div>
